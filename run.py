a5de1539c447d999a922fbcda7770716a1148b50
whoisibk <oyetimehinjoseph@gmail.com>
2025-11-06 11:42:36 +0100

    Rearranged some code

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from extensions import db
 
 app = create_app()
 
-if __name__ == "__main__":
-    with app.app_context():
-       db.create_all()
-    app.run(host="0.0.0.0", debug=True)
+with app.app_context():
+   db.create_all()
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True)
